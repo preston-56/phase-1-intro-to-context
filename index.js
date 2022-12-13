66206731ec423b6b8f8c1a65fb8c43844fe06074
Preston <prestonosoro56@gmail.com>
2022-12-13 19:00:46 +0300

    partial

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
 // Your code here
+// [20 passing]
+ function createEmployeeRecord(employee) {
+  const employeeRecords = {
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  }
+  return employeeRecords;
+ };
+
+//  createEmployeeRecords
+ function createEmployeeRecords(newArr){
+  return newArr.map(createEmployeeRecord);
+ }
+
+//  createTimeInEvent
+let createTimeInEvent = function (employee, dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+  employee.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 8),
+    date,
+  });
+
+  return employee;
+};
+
+// createTimeInEvent
+function createTimeInEvent (employee, dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+
+  employee.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 5),
+    date,
+  });
+  return employee;
+};
+
+// createTimeOutEvent
+
+function createTimeOutEvent (employee, dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+
+  employee.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 5),
+    date,
+  });
+
+  return employee;
+};
+
+// hoursWorkedOnDate
+function hoursWorkedOnDate(employee, recordedDate) {
+  let inEvent = employee.timeInEvents.find(function (e) {
+    return e.date === recordedDate;
+  });
+
+  let outEvent = employee.timeOutEvents.find(function (e) {
+    return e.date === recordedDate;
+  });
+  return (outEvent.hour - inEvent.hour) / 100;  //behavior
+};
